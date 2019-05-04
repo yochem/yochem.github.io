ea7a2204e17588defc29e167fafd6e5abe0401a1
Yochem van Rosmalen <yochem@icloud.com>
2019-05-04 17:35:30 +0200

    Check which color mode is prefered by the browser

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+function detectColorMode() {
+    // detect which color mode is set. Currently only works with Safari
+    matcher = window.matchMedia('(prefers-color-scheme: dark)');
+    matcher.addListener(onUpdate);
+    onUpdate();
+}
+
+function onUpdate() {
+    // check which color is prefered by the browser
+    if (matcher.matches) {
+        setColorMode('dark');
+    } else {
+        setColorMode('light');
+    }
+}
+
+function setColorMode(color) {
+    var body = document.body;
+    var text = document.querySelector('p');
+    var button = document.querySelector('button');
+
+    // decide which color to switch to
+    if (color === 'dark') {
+        body.style.backgroundColor = '#000';
+        text.style.color = '#fff';
+        button.innerHTML = '(light version)';
+    } else {
+        body.style.backgroundColor = '#fff';
+        text.style.color = '#000';
+        button.innerHTML = '(dark version)';
+    }
+}
+
 function toggleDark() {
     var body = document.body;
     var text = document.querySelector('p');
