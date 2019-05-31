be5a219d6c798d555dab7aa0e22ce22e45bfd5d3
Yochem van Rosmalen <yochem@icloud.com>
2019-05-31 20:35:36 +0200

    Use <pre> again

--- script.js
+++ script.js
@@ -1,39 +1,6 @@
-function detectColorMode() {
-    // detect which color mode is set. Currently only works with Safari
-    matcher = window.matchMedia('(prefers-color-scheme: dark)');
-    matcher.addListener(onUpdate);
-    onUpdate();
-}
-
-function onUpdate() {
-    // check which color is prefered by the browser
-    if (matcher.matches) {
-        setColorMode('dark');
-    } else {
-        setColorMode('light');
-    }
-}
-
-function setColorMode(color) {
-    var body = document.body;
-    var text = document.querySelector('p');
-    var button = document.querySelector('button');
-
-    // decide which color to switch to
-    if (color === 'dark') {
-        body.style.backgroundColor = '#000';
-        text.style.color = '#fff';
-        button.innerHTML = '(light version)';
-    } else {
-        body.style.backgroundColor = '#fff';
-        text.style.color = '#000';
-        button.innerHTML = '(dark version)';
-    }
-}
-
 function toggleDark() {
     var body = document.body;
-    var text = document.querySelector('p');
+    var text = document.querySelector('pre');
     var button = document.querySelector('button');
 
     // first time loading js thinks backgroundcolor not set
@@ -45,10 +12,10 @@ function toggleDark() {
     if (body.style.backgroundColor === 'rgb(255, 255, 255)') {
         body.style.backgroundColor = '#000';
         text.style.color = '#fff';
-        button.innerHTML = '(light version)';
+        button.innerHTML = '(<u>light version</u>)';
     } else {
         body.style.backgroundColor = '#fff';
         text.style.color = '#000';
-        button.innerHTML = '(dark version)';
+        button.innerHTML = '(<u>dark version</u>)';
     }
 }
