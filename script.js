4fcb6639438475e36f3472be7408ff1d388e178f
Yochem van Rosmalen <yochem@icloud.com>
2019-11-17 12:33:12 +0100

    Add support for prefers-color-mode media query

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
-function toggleDark() {
+function detectColorMode() {
+    // detect which color mode is set. Currently only works with Safari
+    matcher = window.matchMedia('(prefers-color-scheme: dark)');
+    matcher.addListener(onUpdate);
+    onUpdate();
+}
+
+function onUpdate() {
+    // check which color is prefered by the browser
+    if (matcher.matches) {
+        setColorMode('dark');
+    } else {
+        setColorMode('light');
+    }
+}
+
+function setColorMode(color) {
     var body = document.body;
     var text = document.querySelector('div');
     var button = document.querySelector('button');
 
-    // first time loading js thinks backgroundcolor not set
-    if (!body.style.backgroundColor) {
-        body.style.backgroundColor = '#fff';
+    var darkColor = '#1c1c1c';
+    var white = '#fff';
+
+    if (color === 'dark') {
+        body.style.backgroundColor = darkColor;
+        text.style.color = white;
+        button.innerHTML = '(<u>light version</u>)';
+    } else {
+        body.style.backgroundColor = white;
+        text.style.color = darkColor;
+        button.innerHTML = '(<u>dark version</u>)';
     }
+}
+
+function toggleDark() {
+    var body = document.body;
 
     // decide which color to switch to
     if (body.style.backgroundColor === 'rgb(255, 255, 255)') {
-        body.style.backgroundColor = '#1c1c1c';
-        text.style.color = '#fff';
-        button.innerHTML = '(<u>Light version</u>)';
+        setColorMode('dark');
     } else {
-        body.style.backgroundColor = '#fff';
-        text.style.color = '#1c1c1c';
-        button.innerHTML = '(<u>Dark version</u>)';
+        setColorMode('light');
     }
 }
