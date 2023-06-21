7ef30e577fadbe64395085dc2644f072984cbf62
Yochem van Rosmalen <git@yochem.nl>
2023-06-21 16:06:53 +0200

    Toggle Dark/Light mode!
    Also remembers your setting via local storage and check the browser
    preference.

---
+++ assets/js/darkmode.js
@@ -0,0 +1,45 @@
+(function setupDarkMode() {
+	const darkModePreference = window.matchMedia("(prefers-color-scheme: dark)");
+	darkModePreference.addEventListener("change", e => e.matches && toggleDarkMode());
+
+	const storedDarkMode = localStorage.getItem('dark-mode');
+	if (!!storedDarkMode) {
+		// convert from string to bool
+		setDarkMode(storedDarkMode === 'true');
+		return;
+	}
+
+	if (darkModePreference.matches) {
+		setDarkMode(true);
+	}
+
+	localStorage.setItem('dark-mode', darkModePreference.matches);
+})();
+
+function setDarkMode(dark) {
+	const body = document.getElementsByTagName('body')[0];
+	const img = document.getElementById('darkmode-toggle').children[0];
+	const themeColor = document.querySelector("meta[name='theme-color']")
+
+	if (dark) {
+		img.setAttribute('src', 'sun.png');
+		body.classList.add('dark');
+
+        themeColor.setAttribute("content", '#1b1b1b');
+	} else {
+		img.setAttribute('src', 'moon.png');
+		body.classList.remove('dark');
+
+        themeColor.setAttribute("content", 'grey');
+
+	}
+	localStorage.setItem('dark-mode', dark);
+}
+
+
+function toggleDarkMode() {
+	const body = document.getElementsByTagName('body')[0];
+	const isDarkModeEnabled = body.classList.contains('dark');
+
+	setDarkMode(!isDarkModeEnabled);
+}
