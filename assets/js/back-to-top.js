76dffdfff28924a1b9f1f5e89697f6ba8b2e957b
Yochem van Rosmalen <git@yochem.nl>
2023-08-26 17:00:28 +0200

    Remove user-option to switch color scheme
    It now uses the preferred color scheme. No way to change.

--- assets/js/back-to-top.js
+++ assets/js/back-to-top.js
@@ -1,7 +1,7 @@
 const browserHeight = document.documentElement.clientHeight;
 const bodyHeight = document.getElementsByTagName('body')[0].clientHeight;
 
-if  (bodyHeight > browserHeight) {
+if (bodyHeight > browserHeight) {
 	document.getElementsByTagName('nav')[0].insertAdjacentHTML(
 		'afterbegin',
 		'<a href="#">Top</a> | '
