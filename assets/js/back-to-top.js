fec4dab93497548f86b8ce116d41d2321729e5d0
Yochem van Rosmalen <git@yochem.nl>
2024-04-09 16:27:36 +0200

    Include scroll-to-top js in html

--- assets/js/back-to-top.js
+++
@@ -1,6 +0,0 @@
-const browserHeight = document.documentElement.clientHeight;
-const bodyHeight = document.getElementsByTagName('body')[0].clientHeight;
-
-if (bodyHeight > browserHeight) {
-	document.getElementById('totop').style.display = 'initial';
-}
